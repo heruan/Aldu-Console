9a9cf2638a4b5746bff949a4e9a054466ba8364d
Giovanni Lovato <heruan@aldu.net>
2012-10-02 18:52:33 +0200

    Aldu\Console

---
+++ Controllers/Console.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Aldu\Console\Controllers\Console
+ *
+ * AlduPHP(tm) : The Aldu Network PHP Framework (http://aldu.net/php)
+ * Copyright 2010-2012, Aldu Network (http://aldu.net)
+ *
+ * Licensed under Creative Commons Attribution-ShareAlike 3.0 Unported license (CC BY-SA 3.0)
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author        Giovanni Lovato <heruan@aldu.net>
+ * @copyright     Copyright 2010-2012, Aldu Network (http://aldu.net)
+ * @link          http://aldu.net/php AlduPHP(tm) Project
+ * @package       Aldu\Console\Controllers
+ * @since         AlduPHP(tm) v1.0.0
+ * @license       Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+ */
+
+namespace Aldu\Console\Controllers;
+use Aldu\Core;
+
+class Console extends Core\Controller
+{
+
+  public function main()
+  {
+    if (!$this->model->authorized($this->request->aro, __FUNCTION__)) {
+      return $this->response->status(401);
+    }
+    $stdin = '';
+    $stdout = '';
+    $data = $this->request->data(get_class($this->model));
+    foreach ($data as $console) {
+      $stdin = $console['stdin'];
+      $stdout = $this->evaluate($stdin);
+    }
+    return $this->view->main($stdin, $stdout);
+  }
+
+  private function evaluate($stdin)
+  {
+    ob_start();
+    eval($stdin);
+    return ob_get_clean();
+  }
+}
---
+++ Views/Console.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Aldu\Console\Views\Console
+ *
+ * AlduPHP(tm) : The Aldu Network PHP Framework (http://aldu.net/php)
+ * Copyright 2010-2012, Aldu Network (http://aldu.net)
+ *
+ * Licensed under Creative Commons Attribution-ShareAlike 3.0 Unported license (CC BY-SA 3.0)
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author        Giovanni Lovato <heruan@aldu.net>
+ * @copyright     Copyright 2010-2012, Aldu Network (http://aldu.net)
+ * @link          http://aldu.net/php AlduPHP(tm) Project
+ * @package       Aldu\Console\Views
+ * @since         AlduPHP(tm) v1.0.0
+ * @license       Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+ */
+
+namespace Aldu\Console\Views;
+use Aldu\Core;
+use Aldu\Core\View\Helper;
+
+class Console extends Core\View
+{
+
+  public function main($stdin = '', $stdout = '')
+  {
+    switch ($this->render) {
+    case 'cli':
+      $this->response->type('text');
+      return $this->response->body($stdout);
+    default:
+      $page = Helper\HTML\Page::instance();
+      $page->theme();
+      $form = new Helper\HTML\Form($this->model, __FUNCTION__);
+      if ($stdout) {
+        $_console = $form->textarea('stdout')->node('textarea');
+        $_console->readonly = true;
+        $_console->data('mode', 'text/x-php');
+        $_console->text($stdout);
+      }
+      $console = $form
+        ->textarea('stdin', array(
+          'readonly' => false
+        ))->node('textarea');
+      $console->data('mode', 'text/x-php');
+      $console->text($stdin);
+      $form
+        ->submit('execute',
+          array(
+            'title' => $this->locale->t('Evaluate')
+          ));
+
+      return $this->response->body($page->compose($form));
+    }
+  }
+}
